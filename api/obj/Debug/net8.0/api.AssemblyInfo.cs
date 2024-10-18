16fdd0849d71fced45699edea3848dc1956c5cad
Kavindu Kaveesha <kavindukaveesha16@gmail.com>
2024-10-18 10:50:28 +0530

    pasith branch

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f72393c9d06b48718baa6a96f4aeb22f3a253398")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dccac8dd96393a6209b35811c0aa4f29d6d569a")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
