b916df72ed3fef5eef182fd6efc6d591398f1ef2
Kavindu Kaveesha <kavindukaveesha16@gmail.com>
2024-11-28 20:11:26 +0530

    first part finish

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72f560f4a42ebcfe17913a37002d6638f1297217")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb47fe4d50bb7c1982d8aed7ada1b39c6fb762ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
